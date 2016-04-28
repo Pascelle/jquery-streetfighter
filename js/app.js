efea286385540be49315c85e0e9108faece389ac
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-04-28 15:37:03 -0400

    latest

--- js/app.js
+++ js/app.js
@@ -1,46 +1,46 @@
-$(document).ready(function(){
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show().animate( 
-		{'left': '1020px'},
-		500,
-		function() {
-			$(this).hide();
-			$(this).css('left','520px');});	
-	})
-	.mouseup(function() {
-		$('.ryu-throwing').hide();
-    	$('.ryu-ready').show();
-		// ryu goes back to his ready position
-	});
-	.keydown(function(e) {
-		if (e.which === 88) {
-		$('.ryu-ready').hide();
-		$('ryu-cool').show();
-		}
-	})
-	.keyup(function(e) {
-		if (e.which === 88) {
-			$('.ryu-cool').hide();
-			$('.ryu-still').show();
-		}
-	}
-	
-	});
+$(document).ready(function () {
+   $('.ryu').mouseenter(function () {
+           $('.ryu-still').hide();
+           $('.ryu-ready').show();
+       })
+       .mouseleave(function () {
+           $('.ryu-ready').hide();
+           $('.ryu-still').show();
+       })
+       .mousedown(function () {
+           playHadouken();
+           $('.ryu-ready').hide();
+           $('.ryu-throwing').show();
+           $('.hadouken').finish().show().animate({
+                   'left': '1020px'
+               },
+               500,
+               function () {
+                   $(this).hide();
+                   $(this).css('left', '520px');
+               });
+       })
+       .mouseup(function () {
+           $('.ryu-throwing').hide();
+           $('.ryu-ready').show();
+           // ryu goes back to his ready position
+       });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+
+}).keydown(function (e) {
+   if (e.which === 88) {
+       $('.ryu-ready').hide();
+       $('.ryu-cool').show();
+   }
+}).keyup(function (e) {
+   if (e.which === 88) {
+       $('.ryu-cool').hide();
+       $('.ryu-still').show();
+   }
+});
+
+function playHadouken() {
+   $('#hadouken-sound')[0].volume = 0.5;
+   $('#hadouken-sound')[0].load();
+   $('#hadouken-sound')[0].play();
 }
-
